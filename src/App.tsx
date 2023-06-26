69063b896c581e975155c4301f570af576dc397d
Yuhyeon0516 <rladbgus0516@naver.com>
2023-06-26 17:39:53 +0900

    Redux Thunk 적용하여 각종 Posts불러오는 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "./reducers";
+import axios from "axios";
 
 type Props = {
   value: any;
@@ -9,12 +10,30 @@ type Props = {
   onDecrement: () => void;
 };
 
+interface Post {
+  userID: number;
+  id: number;
+  title: string;
+}
+
 function App({ value, onIncrement, onDecrement }: Props) {
   const dispatch = useDispatch();
   const counter = useSelector((state: RootState) => state.counter);
   const todos: string[] = useSelector((state: RootState) => state.todos);
-
+  const posts: Post[] = useSelector((state: RootState) => state.posts);
   const [todoValue, setTodoValue] = useState("");
+
+  useEffect(() => {
+    dispatch(fetchPosts());
+  }, [dispatch]);
+
+  const fetchPosts = (): any => {
+    return async function fetchPostsThunk(dispatch: any, getState: any) {
+      const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      dispatch({ type: "FETCH_POSTS", payload: response.data });
+    };
+  };
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTodoValue(e.target.value);
   };
@@ -38,6 +57,11 @@ function App({ value, onIncrement, onDecrement }: Props) {
         <input type="text" value={todoValue} onChange={handleChange} />
         <input type="submit" />
       </form>
+      <ul>
+        {posts.map((post, index) => (
+          <li key={index}>{post.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
